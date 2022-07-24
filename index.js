d6220c57f0b0cf6a60537429b9ef2902eea55bbf
RishabhChoudhary172001 <rishabhchoudhary172001@gmail.com>
2022-07-24 22:11:02 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+// document.addEventLinstener('keydown',function(){
+//   alert.("Hello");
+// })
+let screen=document.getElementById('screen');
+var buttons=document.querySelectorAll('button');
+var screenValue='';
+
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+    buttonText=e.target.innerText;
+
+    if(buttonText=='x'){
+      buttonText = '*';
+      screenValue+= buttonText;
+      screen.value=screenValue;
+    }
+    else if(buttonText=='C'){
+      screenValue= "";
+      screen.value=screenValue;
+    }
+    else if(buttonText == '='){
+      screen.value =eval(screenValue);
+    }
+    else{
+      screenValue+=buttonText;
+      screen.value=screenValue;
+    }
+  });
+}
